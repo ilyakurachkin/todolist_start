2896c00b8e5cafa132be6bf149fbb85b69d6dc0d
ilyakurachkin <patlatov@inbox.ru>
2022-10-23 19:56:15 +0300

    Updates

---
+++ precache-manifest.04caa4df8dd2ea386fcf2c8c712d306b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11426a15240752f020f7fbd7e304451",
+    "url": "/todolist_start/index.html"
+  },
+  {
+    "revision": "cdd7d8fa4cc550b95b74",
+    "url": "/todolist_start/static/css/main.6f30ddfc.chunk.css"
+  },
+  {
+    "revision": "9cb9b538998eff278bb4",
+    "url": "/todolist_start/static/js/2.9d407cf8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist_start/static/js/2.9d407cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd7d8fa4cc550b95b74",
+    "url": "/todolist_start/static/js/main.d4d6f611.chunk.js"
+  },
+  {
+    "revision": "de4936666aea4e24a89d",
+    "url": "/todolist_start/static/js/runtime-main.f9925f21.js"
+  }
+]);
